1b50b27cffae6ff52ca3e00ec611da1b23f2fb2a
Casey Vockrodt <casey.vockrodt@gmail.com>
2019-03-24 21:29:30 -0600

    test: Add tests for crc, cli, and config

--- tests/test_crc.py
+++ tests/test_crc.py
@@ -6,4 +6,4 @@ from putio_downloader import PutioAria2cDownloader
 def test_crc():
     config_path = 'config.ini.example'
     test_instance = PutioAria2cDownloader(rpc_url='http://localhost:6800/rpc')
-    assert test_instance.crc(config_path) == 'ED5B9E9C'
+    assert test_instance.crc(config_path) == '45E4D084'
